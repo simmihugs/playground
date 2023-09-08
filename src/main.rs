ec291bfa1b5eff7b335912b0196359664ea3971f
simmihugs <simon.graetz@gmx.de>
2023-09-08 21:17:19 +0200

    tried out some xml stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::fs::File;
 use std::io::BufReader;
+use std::{fs::File, str::FromStr};
 
-use xml::reader::{EventReader, XmlEvent};
+use xml::{
+    name::OwnedName,
+    reader::{EventReader, XmlEvent},
+};
 
 fn main() -> std::io::Result<()> {
     let file = File::open("file.xml")?;
@@ -11,13 +14,28 @@ fn main() -> std::io::Result<()> {
     let mut depth = 0;
     for e in parser {
         match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+            Ok(XmlEvent::StartElement {
+                name, attributes, ..
+            }) => {
+                println!("{:spaces$}{name}: {{", "", spaces = depth * 2);
+                for attr in attributes {
+                    let name = attr.name.to_string() + ",";
+                    let value = attr.value.to_string();
+                    println!(
+                        "{:spaces$}{{ name: {name:5} value: {value} }}",
+                        "",
+                        spaces = 1 + depth * 4
+                    );
+                }
                 depth += 1;
             }
             Ok(XmlEvent::EndElement { name }) => {
                 depth -= 1;
-                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+                if name != OwnedName::from_str("testcases").unwrap() {
+                    println!("{:spaces$}}},", "", spaces = depth * 2);
+                } else {
+                    println!("{:spaces$}}}", "", spaces = depth * 2);
+                }
             }
             Err(e) => {
                 eprintln!("Error: {e}");

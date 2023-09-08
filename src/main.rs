9cef97de87a22c4a10542f6f6ce3a3627b9e2e01
simmihugs <simon.graetz@gmx.de>
2023-09-08 21:52:13 +0200

    added iced example

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,68 @@
-use std::io::BufReader;
-use std::{fs::File, str::FromStr};
+use iced::Settings;
+use iced::widget::{Button, Column, Container, Text};
+use iced::Sandbox;
+use main_page::MainPage;
 
-use xml::{
-    name::OwnedName,
-    reader::{EventReader, XmlEvent},
-};
+mod main_page;
 
-fn main() -> std::io::Result<()> {
-    let file = File::open("file.xml")?;
-    let file = BufReader::new(file); // Buffering is important for performance
 
-    let parser = EventReader::new(file);
-    let mut depth = 0;
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement {
-                name, attributes, ..
-            }) => {
-                println!("{:spaces$}{name}: {{", "", spaces = depth * 2);
-                for attr in attributes {
-                    let name = attr.name.to_string() + ",";
-                    let value = attr.value.to_string();
-                    println!(
-                        "{:spaces$}{{ name: {name:5} value: {value} }}",
-                        "",
-                        spaces = 1 + depth * 4
-                    );
-                }
-                depth += 1;
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                depth -= 1;
-                if name != OwnedName::from_str("testcases").unwrap() {
-                    println!("{:spaces$}}},", "", spaces = depth * 2);
-                } else {
-                    println!("{:spaces$}}}", "", spaces = depth * 2);
-                }
-            }
-            Err(e) => {
-                eprintln!("Error: {e}");
-                break;
-            }
-            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
-            _ => {}
+fn main() -> Result<(), iced::Error> {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    count: i32,
+    current_view: Views,
+    main_page: MainPage
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Views {
+    Counter,
+    Main
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum CounterMessage {
+    Increment,
+    Decrement,
+    ChangePage(Views)
+}
+
+impl Sandbox for Counter {
+    type Message = CounterMessage;
+
+    fn new() -> Self {
+        Counter { 
+            count: 0,
+            current_view: Views::Counter,
+            main_page: MainPage::new()
         }
     }
 
-    Ok(())
+    fn title(&self) -> String {
+        String::from("Counter app")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            CounterMessage::Increment => self.count += 1,
+            CounterMessage::Decrement => self.count -= 1,
+            CounterMessage::ChangePage(view) => self.current_view = view
+        }
+    }
+
+    fn view(&self) -> iced::Element<Self::Message> {
+        let label = Text::new(format!("Count: {}", self.count));
+        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
+        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
+        let navigate = Button::new(Text::new("Go to the next page")).on_press(CounterMessage::ChangePage(Views::Main));
+        let col = Column::new().push(incr).push(label).push(decr).push(navigate).spacing(5);
+        let counter_layout = Container::new(col).center_x().center_y().width(iced::Length::Fill).height(iced::Length::Fill);
+        
+        match self.current_view {
+            Views::Counter => counter_layout.into(),
+            Views::Main => self.main_page.view()
+        }
+    }
 }
